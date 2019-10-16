24b397a8e634745d510aa2c3c8c498be0f95e17a
lilincong <lidaha.004@hotmail.com>
2019-10-16 20:02:59 +0800

    打包命令

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,8 +1,10 @@
-import sys
+import sys, os
+if hasattr(sys, 'frozen'):
+    os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
 from PyQt5.QtWidgets import QApplication
 
 from Scripts.UI.UIMain import UIMain
-
+#pyinstaller --hidden-import pandas -F -w Scripts/Main.py
 def main():
     app = QApplication(sys.argv)
     uiMain = UIMain()
